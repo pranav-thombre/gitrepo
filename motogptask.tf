4525530e3be6afd8ffeb65e64030732a8039559d
pranav-thombre <pranavthombre0532@gmail.com>
2024-03-06 16:23:49 +0530

    commented motogptask.tf

--- motogptask.tf
+++ motogptask.tf
@@ -1,45 +1,45 @@
-provider "aws" {
-  region = var.region 
-}
+# provider "aws" {
+#   region = var.region 
+# }
 
-resource "aws_instance" "amazonubuntumachine" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name = var.key_name
-  count = var.countt
+# resource "aws_instance" "amazonubuntumachine" {
+#   ami           = var.ami
+#   instance_type = var.instance_type
+#   key_name = var.key_name
+#   count = var.countt
 
- tags = {
-    Name = var.tags
-  }
- }
+#  tags = {
+#     Name = var.tags
+#   }
+#  }
 
-terraform {
-  backend "s3" {
-    bucket                  = "motogpterratask"
-    key                     = "motogp-project"
-    region                  = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket                  = "motogpterratask"
+#     key                     = "motogp-project"
+#     region                  = "ap-south-1"
+#   }
+# }
 
-variable "region" {
-   default = "ap-south-1"
-   description = "DEPLOYMENT IN AWS"
- }
-variable "ami" {
-    default = "ami-03bb6d83c60fc5f7c"
-    description = "SERVER"
- }
-variable "instance_type" {
-    default = "t2.micro"
-    description = "TYPE"
- }
- variable "key_name" {
-    default = "id_rsa"
-    description = "SAME SG FOR ALL"
- }
- variable "countt" {
-    default = "5"
- }
- variable "tags" {
-    default = "motogp"
- }
+# variable "region" {
+#    default = "ap-south-1"
+#    description = "DEPLOYMENT IN AWS"
+#  }
+# variable "ami" {
+#     default = "ami-03bb6d83c60fc5f7c"
+#     description = "SERVER"
+#  }
+# variable "instance_type" {
+#     default = "t2.micro"
+#     description = "TYPE"
+#  }
+#  variable "key_name" {
+#     default = "id_rsa"
+#     description = "SAME SG FOR ALL"
+#  }
+#  variable "countt" {
+#     default = "5"
+#  }
+#  variable "tags" {
+#     default = "motogp"
+#  }
